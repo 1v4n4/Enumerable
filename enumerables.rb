43c8eca495f68983a60adcc18ba691c103ae53d6
TedLivist <livistteddy@gmail.com>
2021-04-22 09:25:42 +0100

    Add my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,20 @@
-module Enumerables
+module Enumerable
   def my_each
     for i in self
       yield(i)
     end
     self
   end
+
+  def my_each_with_index    
+    for i in self
+      yield i, i - 1
+    end
+  end
+end
+
+hash = {:one => 1, :two => 2}
+hash.my_each {|k, v| puts "#{k} => #{v}"}
+
+arr = [1,2,3,4]
+arr.my_each_with_index {|val, indx| puts "Index: #{indx} contains #{val}"}
