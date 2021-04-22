f9e73d863cebbaed1fd02c1f8e5f15953096a062
ivana <ivananl@yahoo.com>
2021-04-22 10:43:09 +0200

    Add selected

--- enumerables.rb
+++ enumerables.rb
@@ -6,15 +6,32 @@ module Enumerable
     self
   end
 
-  def my_each_with_index    
+  def my_each_with_index
     for i in self
       yield i, i - 1
     end
   end
+
+
+  def my_select
+    selected = []
+    for i in self
+      if yield(i)
+      selected.push(i)
+      end
+    end
+    selected
+  end
 end
+
+
 
 hash = {:one => 1, :two => 2}
 hash.my_each {|k, v| puts "#{k} => #{v}"}
 
-arr = [1,2,3,4]
-arr.my_each_with_index {|val, indx| puts "Index: #{indx} contains #{val}"}
+arr = [1, 2, 5, 3, 4]
+p arr.my_select {|i| arr[i] > 1}
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+friends.my_select { |friend| friend != 'Brian' }
