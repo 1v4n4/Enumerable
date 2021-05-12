3024d7cbdc74d5e44d3c772d9a12976809acbd7c
ivana <ivananl@yahoo.com>
2021-05-12 17:25:37 +0200

    Add tests for my_any and my_all

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -4,8 +4,10 @@ describe Enumerable do
 
   let(:arr) {[1,2,3,4,5]}
   let(:hash) {{one: "first", two: "second", three: "third", four: "fourth", five: "fifth"}}
-  let(:arr_nill) {[nil, 1, 2, 3, 4, 5]}
-  let(:arr_false) {[1,2,3,4,5,false]}
+  let(:arr_nil) {[nil, 1, 2, 3, 4, 5]}
+  let(:arr_false) {[1, 2, 3, 4, 5, false]}
+  let(:arr_string) {['one', 'two', 'three', 'four', 'five']}
+  let(:arr_none) {[false, nil, false, nil, false]}
 
   describe '#my_each' do
 
@@ -55,6 +57,59 @@ describe Enumerable do
     it 'call a block, iterate over the argument and update a new argument' do
      new_variable = Array.new
      expect(hash.my_select { |k,v| k == :one}).to eq([[:one, 'first']])
+    end
+  end
+
+  describe '#my_all?' do
+    it 'passes each element of the collection to the given block and returns true if the block never returns false or nil' do
+      expect(arr.my_all? {|x| x<100}).to be true
+    end
+
+    it 'passes each element of the collection to the given block and returns false if the block returns false or nil' do
+      expect(arr.my_all? {|x| x.odd?}).to be false
+    end
+
+    it 'if block not given, returns true when none of the collection members are false or nil' do
+      expect(arr.my_all?).to be true
+    end
+
+    it 'if block not given, returns false when at least one of the collection members are false or nil' do
+      expect(arr_nil.my_all?).to be false
+      expect(arr_false.my_all?).to be false
+    end
+
+    it 'if pattern supplied, returns true if pattern equals each element of collection' do
+      expect(arr_string.my_all? (/t/) ).to be false
+    end
+
+    it 'if pattern supplied, returns false  if pattern is different to at least one element of collection' do
+      expect(arr_string.my_all? (/[a-z]/) ).to be true
+    end
+  end
+
+  describe '#my_any?' do
+    it 'passes each element of the collection to the given block and returns true if gets true value' do
+      expect(arr.my_any? {|x| x==1}).to be true
+    end
+
+    it 'passes each element of the collection to the given block and returns false if gets all false or nil values' do
+       expect(arr.my_any? {|x| x==10}).to be false
+    end
+
+    it 'if block not given returns true if at least one of the collection members is not false or nil' do
+       expect(arr_nil.my_any?).to be true
+    end
+
+    it 'if block not given returns false if all collection members are false or nil' do
+      expect(arr_none.my_any?).to be false
+    end
+
+    it 'if pattern supplied, returns true if pattern equals at least one element in a collection' do
+      expect(arr_string.my_any? (/t/)).to be true
+    end
+
+    it 'if pattern supplied, returns false if pattern is not equal to any element in a collection' do
+      expect(arr_string.my_any? (/[0-9]/)).to be false
     end
 
   end
